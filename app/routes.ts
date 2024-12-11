b04eb1a2bb90029548e0378514caf91e4f0b5289
TJ Hoo <tj.hoo@wezmart.com>
2024-12-11 08:06:16 +0800

    Upgrade

--- app/routes.ts
+++ app/routes.ts
@@ -1,4 +1,6 @@
 import type { RouteConfig } from "@react-router/dev/routes";
 import { index } from "@react-router/dev/routes";
 
-export const routes: RouteConfig = [index("routes/home.tsx")];
+const routes: RouteConfig = [index("routes/home.tsx")];
+
+export default routes;
