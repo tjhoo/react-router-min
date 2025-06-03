c464b3142c97acc4f8062bfddd1e0ceec5913ce1
TJ Hoo <tj.hoo@wezmart.com>
2025-06-03 14:46:41 +0800

    Links component is required to load CSS in production build

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
-import { Outlet, Scripts, ScrollRestoration } from "react-router";
-import "./tailwind.css";
+import { Links, Outlet, Scripts, ScrollRestoration } from "react-router";
+import "./app.css";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -7,6 +7,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Links />
       </head>
       <body>
         {children}
